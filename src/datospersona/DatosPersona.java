02211d42f9c6d07f7c8f3a3d9c929589cf001215
ufg <ufg@ufg-HP>
2018-03-15 16:06:03 -0600

    Completo

--- src/datospersona/DatosPersona.java
+++ src/datospersona/DatosPersona.java
@@ -16,6 +16,10 @@ public class DatosPersona {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println(" nombre: Mercedes Alejandra Padilla Estrada\n "
+                + "fecha nac: 26/11/97\n "
+                + "carnet: 00186416\n edad: 20\n sexo: mujer\n estado: soltera\n"
+                + " carrera: ing. informatica\n annio: 3°\n");
     }
     
 }
